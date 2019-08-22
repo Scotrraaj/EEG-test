d65971994023e6c0516aaa21eb336c4c411779c6
Scotrraaj <54401236+Scotrraaj@users.noreply.github.com>
2019-08-22 23:55:21 +0200

    Cleaning up all the blas

--- random_testing.py
+++ random_testing.py
@@ -26,9 +26,7 @@ for root, dirs, files in os.walk(r'S:\Testing Python\reports'):           #for l
             path1= os.path.join(root, file)
             wordcheck(path1 , ["alzheimer"])
 
-            blabla
-            2nd bla bla
-
+            cleaning up unnecessary bla blas
 
 #########################
 """
