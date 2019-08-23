663f79827209b8deecd6841e7a9ec05839f269aa
Scotrraaj <scotgopal@gmail.com>
2019-08-23 17:31:24 +0200

    Addition of more words into the search engine to scan for more hits.

--- random_testing.py
+++ random_testing.py
@@ -5,19 +5,17 @@ def wordcheck(filename, listwords):
         file = open(filename,'r', encoding='utf8')
         read = file.readlines()
         file.close()
+        longline = ''.join(read)
+        longline=longline.lower()
+        found = False
         for word in listwords:
             word = word.lower()
-            count2 = 0
-            for sentence in read:
-                sentence = sentence.lower()
-                if word in sentence:
-                    count2+=1
-                    continue
-            if count2 >= 1:
-                #print('Found approx. %d in %s' %(count2, filename))
+            if word in longline:
                 matches.append('1')
-            else:
-                matches.append('0')
+                found = True
+                break
+        if not found:
+            matches.append('0')
     except FileExistsError:
         print("The file is not there!")
 
@@ -26,25 +24,28 @@ import pandas as pd
 
 count=0
 
-session_records_df = pd.DataFrame()
 file_path = []
 matches = []
 for root, dirs, files in os.walk(r'S:\Testing Python\reports'):
-    #for loop to 'walk' around the directory
     for file in files:
-        #a for loop to check every file in the subdirectories
         if file.endswith('.txt'):
-            count+=1#setting up a condition for reading only textfiles
+            count+=1
             path1= os.path.join(root, file)
             file_path.append(path1)
-            wordcheck(path1 , ["alzheimer"])
-session_records_df["File Path"] = file_path
-session_records_df["matches_keyword"] = matches
-print('TOTAL NUMBER OF TEXT FILES:', count)
-print(session_records_df)
-print(session_records_df.loc[session_records_df["matches_keyword"]=='1'])
+            wordcheck(path1 , ["alzheimer", "dementia" , "donepezil" , ""])
 
-session_records_df.to_csv(r'S:\Testing Python\Filtered.csv')
+#session_records_df = pd.DataFrame({'File Path': file_path},{'matches_keyword': matches})
+print('TOTAL NUMBER OF TEXT FILES:', count)
+tempdf=pd.DataFrame({'Matches': matches})
+print(tempdf)
+print('\n')
+print(tempdf.loc[tempdf["Matches"]=='1'])
+#df = pd.DataFrame({'File Path': file_path , 'Match' : matches})
+#print(df)
+#print(session_records_df)
+#print(session_records_df.loc[session_records_df["matches_keyword"]=='1'])
+
+#session_records_df.to_csv(r'S:\Testing Python\Filtered.csv')
 
 
 #########################
@@ -66,10 +67,6 @@ else:
 
 - item
 - item
-
-
-
-
 return has_epilepsy
 
 storingdata = pd.DataFrame({paths:listoffiles, has })
